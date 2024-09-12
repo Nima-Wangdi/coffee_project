6a718ded3a31a0378515756cb9c4b545846541a3
Nima-Wangdi <96816776+Nima-Wangdi@users.noreply.github.com>
2024-09-12 10:46:12 -0700

    cofee project
    Cofee_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_project/pages/cover.dart';
+import 'package:coffee_project/pages/sign_up_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:CoverPages(),
+      // home:CoverPages(),
+      home: SignUpPage(),
     );
   }
 }
